93deb8d7515df7e8395d3641e0bfd285517b0a47
Younes Bouaziz <54204783+yb74@users.noreply.github.com>
2024-02-29 18:18:15 +0100

    implementing service discovery 'Netflix Eureka' and API gateway with load balancing

--- inventory-service/src/main/java/com/younesbouaziz/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/younesbouaziz/inventoryservice/InventoryServiceApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+/**
+ *  "@EnableEurekaClient" is deprecated, no need to annotate the main class.
+ *  It is enough to add the spring-cloud-starter-netflix-eureka-client
+ *  dependency to pom.xml and if we have the application name in yml
+ *  or properties file it will be registered to Eureka Server.
+ */
 @SpringBootApplication
 public class InventoryServiceApplication {
 
